6e9a10a92e0f1afca6e54f67131b0a1e7a3d764f
Immanuel John <john@digiledge.com>
2018-10-07 14:36:46 +0530

    second

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router()
+var service = require('../services/ownership')
 
-router.post('/create',()=>{
+router.post('/create',(req,res)=>{
     //Create ownership
+    service.create(req).then((response)=>{
+        res.status(200).send(response)
+    })
+
+
 })
 
 router.get('/read',()=>{
     //Read ownership
+    service.read(req).then((response)=>{
+        res.status(200).send(response)
+    })
 })
 
 module.exports = router
--- services/ownership.js
+++ services/ownership.js
@@ -1,9 +1,17 @@
 var ownershipService = {
-    create: function(){
+    create: function(req){
         //
+        return new Promise((resolve,reject)=> {
+            resolve(req);
+        })
     },
 
-    read: function(){
+    read: function(req){
         //
+        return new Promise((resolve,reject)=> {
+            resolve(req);
+        })
     }
 }
+
+module.exports = ownershipService
